8d04c354596b50f1f4b45440a6572ba91115532f
mitja.lenic@gmail.com <mitja.lenic@gmail.com@452442e3-3709-ca46-648c-f37f2b502962>
2012-04-20 00:05:42 +0000

    Compatibility branch with Lucene 2.4 submitted by Matt Ball <mattjball@gmail.com>

---
+++ branches/lucene-2.4/LuceneTransform/test/org/apache/lucene/store/transform/MeasureTimes.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.apache.lucene.store.transform;
+
+import java.io.IOException;
+import org.junit.Test;
+
+/**
+ *
+ * @author mile4386
+ */
+public class MeasureTimes {
+
+    @Test
+    public void batchTest() throws Exception {
+        singleBatchTest(4 * 1024, true);
+        singleBatchTest(16*1024, true);
+        singleBatchTest(128 * 1024, true);
+        singleBatchTest(4 * 1024, false);
+        singleBatchTest(16 * 1024, false);
+        singleBatchTest(128 * 1024, false);
+    }
+
+    public static void main(String args[]) throws IOException, Exception {
+
+        MeasureTimes t = new MeasureTimes();
+        t.batchTest();
+    }
+
+    private void singleBatchTest(int chunkSize, boolean directStore) throws Exception {
+        TransformTest tt = new TransformTest();
+        tt.setChunkSize(chunkSize);
+        tt.setDirectStore(directStore);
+        for (int i = 0; i < 31; i++) {
+  //          System.out.println("Run cs=" + (chunkSize / 1024) + "k directStore=" + directStore + " run=" + i);
+            tt.setUp();
+            tt.lucene();
+            tt.setUp();
+            tt.luceneNull();
+            tt.setUp();
+            tt.compressed();
+            tt.setUp();
+            tt.encrypted();
+            tt.setUp();
+            tt.encryptedCBC();
+            tt.setUp();
+            tt.compressedEncryptedCBC();
+            if (i == 0) {
+                // clear statistics to prevent startup influence
+//                System.out.println(tt.getStatistics());
+                tt.getStatistics().clear();
+            } else {
+  //              System.out.println(tt.getStatistics());
+            }
+        }
+        System.out.println("Chunk size " + (chunkSize / 1024) + "k logAppend=" + directStore);
+        System.out.println(tt.getStatistics());
+    }
+}
